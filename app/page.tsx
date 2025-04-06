309426b391fb901ed49d1ee21595cf922baab463
Keshav Shenoy <keshavsy@gmail.com>
2025-04-05 23:59:39 -0400

    Fix some typos and add calendly link

--- app/page.tsx
+++ app/page.tsx
@@ -49,7 +49,7 @@ export default function Home() {
             </p>
             
             <p className="text-gray-300">
-              Excited to talk about any of this stuff or anything else. Can be reached at keshavsy[at]gmail[dot]com or on LinkedIn below.
+              Excited to talk about any of this stuff or anything else. Can be reached at keshavsy[at]gmail[dot]com or <a href="https://calendly.com/keshavsy/30min" target="_blank" rel="noopener noreferrer" className="text-blue-400">schedule a call with me here</a> or on LinkedIn below.
             </p>
             
             <div className="flex gap-4 pt-2">
